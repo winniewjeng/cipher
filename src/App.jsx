ee444f9dee80457d732eb7577b9e2769d85d4134
winniewjeng <winniewjeng@gmail.com>
2020-06-23 14:33:36 -0700

    add interface

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const { TextArea, Search } = Input;
 
 function checkStringUniqueness(str) {
   let set = new Set();
-  for(let i = 0; i < str.length; ++i) {
-    if(set.has(str[i])) {
+  for (let i = 0; i < str.length; ++i) {
+    if (set.has(str[i])) {
       return false
     }
     set.add(str[i]);
@@ -20,8 +20,8 @@ function checkStringUniqueness(str) {
 
 function checkStringCharBound(str) {
   const possibleKeyChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  for(let i = 0; i < str.length; ++i) {
-    if(!possibleKeyChars.includes(str[i])) {
+  for (let i = 0; i < str.length; ++i) {
+    if (!possibleKeyChars.includes(str[i])) {
       return false;
     }
   }
@@ -32,18 +32,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      key: null,
+      key: "",
+      msg: "",
+      encrypted: "",
     }
   }
 
   handleUserKeyInput = (e) => {
     const str = e.target.value;
     // check if e.target.value has dup char and if e.target.value is within valid bound
-    // and update this.setState
-    if(checkStringUniqueness(str) && checkStringCharBound(str)) {
-      this.setState( {key: str} ); 
+    // and update this.setState. If reqs aren't met, then don't update key
+    if (checkStringUniqueness(str) && checkStringCharBound(str)) {
+      this.setState({ key: str });
     }
-    // if reqs aren't met, then don't update key
   }
 
   handleKeyGeneration = () => {
@@ -53,7 +54,18 @@ class App extends React.Component {
     });
   }
 
+  handleUserMessage = (v) => {
+    const msg = v.target.value;
+    // if msg is not a falsy value, setState msg 
+    if (msg) {
+      this.setState({ msg: msg });
+    }
+
+  }
+
   render() {
+    const encrypted = Library.encrypt(this.state.msg, this.state.key);
+
     console.log(this.state.key);
     return (
       <Layout>
@@ -66,30 +78,41 @@ class App extends React.Component {
             <Col flex={'1 1 auto'}>
               <Input
                 placeholder="key"
-                value = { this.state.key ? this.state.key : "" }
-                onChange={(e) => this.handleUserKeyInput(e) }
+                value={this.state.key ? this.state.key : ""}
+                onChange={(e) => this.handleUserKeyInput(e)}
+              // TODO: SHOW WARNING IF KEY INVALID
               ></Input>
             </Col>
             <Col flex={'0 1 0'}>
-              <Button 
+              <Button
                 type="primary"
-                onClick={ this.handleKeyGeneration.bind(this) }
+                onClick={this.handleKeyGeneration.bind(this)}
               >Generate Random Key</Button>
+            </Col>
+            <Col flex={'0 1 0'}>
+              <Button
+                onClick={ ()=> this.setState( { msg:"" } )}
+              >Clear Message</Button>
             </Col>
           </Row>
           <Row>
             Message
           </Row>
           <Row>
-            <TextArea rows={2} placeholder="message" />
+            <TextArea
+              rows={2} placeholder="message"
+              value={ this.state.msg ? this.state.msg : ""}
+              onChange={(e) => this.handleUserMessage(e)} />
           </Row>
           <Row>
             Cryptomessage
           </Row>
           <Row>
-            <TextArea rows={2} placeholder="" />
+            <TextArea rows={2}
+              placeholder="encrypted message"
+              value={encrypted ? encrypted : ""}
+            />
           </Row>
-
 
         </Content>
         <Footer style={{ textAlign: "center" }}> Winnie Jeng © 2020 </Footer>
